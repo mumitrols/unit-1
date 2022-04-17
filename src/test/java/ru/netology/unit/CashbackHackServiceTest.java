715ac4c7b1d3e68235ac55794630f05a7fed80a5
mumitrols <mumitrols@bk.ru>
2022-04-17 11:14:50 +0300

    Скорректирован код сервиса (убрала обработку ошибки, чтоб падал тест)

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -27,4 +27,15 @@ public class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
 }

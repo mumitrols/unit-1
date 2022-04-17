41e16a543cbc4945728bb9baec592453906bf925
mumitrols <mumitrols@bk.ru>
2022-04-17 11:29:24 +0300

    Скорректирован код сервиса (убрала обработку ошибки, теперь один из тестов падает)

--- src/main/java/ru/netology/unit/CashbackHackService.java
+++ src/main/java/ru/netology/unit/CashbackHackService.java
@@ -4,11 +4,6 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        if ( amount <= 1000 ) {
-            return boundary - amount;
-        }
-        else {
-            return boundary - amount % boundary;
-        }
+        return boundary - amount % boundary;
     }
 }
